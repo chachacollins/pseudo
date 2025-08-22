ffbc7e1ee3cb7e44d4a887ace0d4b44678d2cee2
chachacollins <collinschacha@hotmail.com>
2025-08-22 05:44:44 +0300

    feat: lexer beginning

---
+++ src/lexer.rs
@@ -0,0 +1,142 @@
+#[derive(PartialEq, Debug)]
+enum TokenKind {
+    Colon,
+    LParen,
+    RParen,
+    Semicolon,
+    Func,
+    Start,
+    Stop,
+    Write,
+    Return,
+    I32,
+    Number(String),
+    String(String),
+    Ident(String),
+    Eof,
+    Illegal,
+}
+
+#[derive(Debug)]
+pub struct Token {
+    kind: TokenKind,
+    line: usize,
+}
+
+pub struct Lexer {
+    source: Vec<char>,
+    pos: usize,
+    read_pos: usize,
+    line: usize,
+}
+
+impl Lexer {
+    pub fn new(source: String) -> Self {
+        return Self {
+            source: source.chars().collect(),
+            line: 1,
+            pos: 0,
+            read_pos: 0,
+        };
+    }
+
+    fn peek(&self) -> char {
+        if self.read_pos >= self.source.len() {
+            return '\0';
+        }
+        return self.source[self.read_pos];
+    }
+
+    fn advance(&mut self) -> char {
+        if self.read_pos >= self.source.len() {
+            return '\0';
+        }
+        self.pos = self.read_pos;
+        self.read_pos += 1;
+        return self.source[self.pos];
+    }
+
+    fn skip_whitespace(&mut self) {
+        loop {
+            let c = self.peek();
+            match c {
+                '\t' | ' ' | '\r' => {
+                    let _ = self.advance();
+                }
+                '\n' => {
+                    self.line += 1;
+                    let _ = self.advance();
+                }
+                _ => break,
+            }
+        }
+    }
+
+    fn make_token(&self, token_kind: TokenKind) -> Token {
+        Token {
+            kind: token_kind,
+            line: self.line,
+        }
+    }
+
+    fn classify_ident(&self, ident: &str) -> TokenKind {
+        match ident {
+            "start" => TokenKind::Start,
+            "stop" => TokenKind::Stop,
+            "func" => TokenKind::Func,
+            "i32" => TokenKind::I32,
+            "write" => TokenKind::Write,
+            "return" => TokenKind::Return,
+            _ => TokenKind::Ident(ident.to_string()),
+        }
+    }
+
+    fn next_token(&mut self) -> Token {
+        self.skip_whitespace();
+        let c = self.advance();
+        match c {
+            ':' => self.make_token(TokenKind::Colon),
+            ';' => self.make_token(TokenKind::Semicolon),
+            '(' => self.make_token(TokenKind::LParen),
+            ')' => self.make_token(TokenKind::RParen),
+            '0'..='9' => {
+                let mut num = String::new();
+                num.push(c);
+                while self.peek().is_ascii_digit() {
+                    num.push(self.advance());
+                }
+                self.make_token(TokenKind::Number(num))
+            }
+            'a'..='z' | 'A'..='Z' | '_' => {
+                let mut ident = String::new();
+                ident.push(c);
+                while self.peek().is_alphanumeric() {
+                    ident.push(self.advance());
+                }
+                self.make_token(self.classify_ident(&ident))
+            }
+            '\0' => self.make_token(TokenKind::Eof),
+            '"' => {
+                let mut string = String::new();
+                while self.peek() != '"' {
+                    string.push(self.advance());
+                }
+                let _ = self.advance();
+                self.make_token(TokenKind::String(string))
+            }
+            _ => self.make_token(TokenKind::Illegal),
+        }
+    }
+}
+
+impl Iterator for Lexer {
+    type Item = Token;
+    fn next(&mut self) -> Option<<Self as Iterator>::Item> {
+        let next_token = self.next_token();
+        if next_token.kind == TokenKind::Eof {
+            None
+        } else {
+            Some(next_token)
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+mod lexer;
+
+use lexer::Lexer;
+use std::{env, fs, process};
+
 fn main() {
-    println!("Hello, world!");
+    let args = env::args().collect::<Vec<String>>();
+    if args.len() < 2 {
+        eprintln!("[ERROR]: not enough arguements passed");
+        println!("[USAGE]: pseudo <filename> [-o <output_name>]");
+        process::exit(1);
+    }
+    let file_name = &args[1];
+    let source = fs::read_to_string(file_name)
+        .map_err(|err| eprintln!("Could not open file: {file_name} because of {}", err))
+        .unwrap();
+    let lexer = Lexer::new(source);
+    for token in lexer {
+        println!("{:?}", token);
+    }
 }
