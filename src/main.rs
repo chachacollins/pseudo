ce34c3ee746fc92423872daf18d0a9bb874d5847
chachacollins <collinschacha@hotmail.com>
2025-08-31 15:07:19 +0300

    refactor: changed how the cli works

--- src/main.rs
+++ src/main.rs
@@ -6,14 +6,15 @@ use lexer::Lexer;
 use std::process::{self, Command};
 use std::{env, fs};
 
-fn cli_error(msg: &str) -> ! {
-    eprintln!("[ERROR]: {msg}");
-    process::exit(1)
-}
-
 //TODO: ADD ALL THE OPTIONS
 fn print_usage() {
     println!("[USAGE]: pseudo <input> [-o <output>] [OPTIONS]");
+}
+
+fn cli_error(msg: &str) -> ! {
+    eprintln!("[ERROR]: {msg}");
+    print_usage();
+    process::exit(1)
 }
 
 #[derive(Default)]
@@ -53,18 +54,13 @@ fn main() {
     let input_file_path = &args[0];
     let mut output_file_path = None;
 
-    let args = &args[1..];
-    //NOTE: THIS SKIP STUFF IS SUPER UGLY MAYBE CHANGE IT LATER
-    let mut skip = 0;
-    for (i, arg) in args.iter().enumerate() {
+    let mut args = args[1..].iter();
+    while let Some(arg) = args.next() {
         match arg.as_str() {
-            "-o" => {
-                if i + 1 >= args.len() {
-                    cli_error("file output path should be specified after the -o flag");
-                }
-                output_file_path = Some(args[i + 1].as_str());
-                skip = i + 1;
-            }
+            "-o" => match args.next() {
+                Some(path) => output_file_path = Some(path.clone()),
+                None => cli_error("file output path should be specified after the -o flag"),
+            },
             "--help" => {
                 print_usage();
             }
@@ -75,9 +71,6 @@ fn main() {
                 compiler_ctx.keep_ir_output = true;
             }
             arg => {
-                if skip == i {
-                    continue;
-                }
                 cli_error(&format!("Unknown arguement {arg} provided. See --help"));
             }
         }
@@ -88,7 +81,8 @@ fn main() {
             input_file_path
                 .split('.')
                 .next()
-                .unwrap_or_else(|| cli_error("use the .pseudo extension")),
+                .unwrap_or_else(|| cli_error("use the .pseudo extension"))
+                .to_string(),
         );
     }
     let source = match fs::read_to_string(input_file_path) {
@@ -103,12 +97,16 @@ fn main() {
         .unwrap_or_else(|err| cli_error(&format!("could not generate c code {err}")));
     let c_file_path = format!(
         "{}.c",
-        output_file_path.expect("There should be a valid output file here")
+        output_file_path
+            .as_ref()
+            .expect("There should be a valid output file here")
     );
     fs::write(&c_file_path, code).unwrap_or_else(|err| {
         cli_error(&format!("could not write generated c code to file {err}"))
     });
     compiler_ctx.c_file_path = &c_file_path;
-    compiler_ctx.output_path = output_file_path.expect("There should be a valid output file here");
+    compiler_ctx.output_path = output_file_path
+        .as_ref()
+        .expect("There should be a valid output file here");
     compile_c_code(compiler_ctx);
 }
