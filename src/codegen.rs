8e59e31f15447c70617ee8573b847638c8d97dc8
chachacollins <collinschacha@hotmail.com>
2025-09-22 14:18:59 +0300

    refactor: made the codegen interface/api better

--- src/codegen.rs
+++ src/codegen.rs
@@ -1,57 +1,113 @@
 use crate::ir::{CParam, CType, CValue, Cir};
-use std::fmt::{Result, Write};
+use std::fmt::{self, Write};
 
-fn generate_prelude(sink: &mut impl Write) -> Result {
-    writeln!(sink, "#include <stdio.h>")?;
-    writeln!(sink, "#include <stdint.h>")?;
-    Ok(())
+pub struct CodeGen {
+    sink: String,
 }
 
-fn generate_write_stmt(sink: &mut impl Write, ctype: &CType, cvalue: &CValue) -> Result {
-    let write_value = match ctype {
-        CType::Int => {
-            format!("\"%d\", {cvalue}")
+impl CodeGen {
+    pub fn new() -> Self {
+        Self {
+            sink: String::new(),
         }
-        CType::Uint => {
-            format!("\"%u\", {cvalue}")
+    }
+    fn generate_prelude(self: &mut Self) -> fmt::Result {
+        writeln!(self.sink, "#include <stdio.h>")?;
+        writeln!(self.sink, "#include <stdint.h>")?;
+        Ok(())
+    }
+
+    fn generate_write_stmt(self: &mut Self, ctype: &CType, cvalue: &CValue) -> fmt::Result {
+        let write_value = match ctype {
+            CType::Int => {
+                format!("\"%d\", {cvalue}")
+            }
+            CType::Uint => {
+                format!("\"%u\", {cvalue}")
+            }
+            CType::String => {
+                format!("\"%s\", {cvalue}")
+            }
+        };
+        writeln!(self.sink, "printf({write_value});")?;
+        Ok(())
+    }
+
+    fn generate_return_stmt(self: &mut Self, cvalue: &CValue) -> fmt::Result {
+        writeln!(self.sink, "return {cvalue};")?;
+        Ok(())
+    }
+
+    fn generate_subprogdef_stmt(
+        self: &mut Self,
+        name: String,
+        cparams: Vec<CParam>,
+        return_type: &CType,
+        stmts: Vec<Cir>,
+    ) -> fmt::Result {
+        write!(self.sink, "{return_type} {name}")?;
+
+        write!(self.sink, "(")?;
+        let param_strings: Vec<String> = cparams
+            .iter()
+            .map(|param| format!("{} {}", param.param_type, param.name))
+            .collect();
+        write!(self.sink, "{}", param_strings.join(", "))?;
+        write!(self.sink, ")")?;
+
+        writeln!(self.sink, "{{")?;
+        self.generate_stmts(stmts)?;
+        writeln!(self.sink, "}}")?;
+        Ok(())
+    }
+
+    fn generate_if_stmt(self: &mut Self, expr: CValue, stmts: Vec<Cir>) -> fmt::Result {
+        writeln!(self.sink, "if ({expr}) {{")?;
+        self.generate_stmts(stmts)?;
+        writeln!(self.sink, "}}")?;
+        Ok(())
+    }
+
+    fn generate_else_stmt(self: &mut Self, stmts: Vec<Cir>) -> fmt::Result {
+        writeln!(self.sink, "}}")?;
+        writeln!(self.sink, "else {{")?;
+        self.generate_stmts(stmts)?;
+        Ok(())
+    }
+
+    fn generate_stmts(self: &mut Self, stmts: Vec<Cir>) -> fmt::Result {
+        for stmt in stmts {
+            match stmt {
+                Cir::Write(ctype, cvalue) => self.generate_write_stmt(&ctype, &cvalue)?,
+                Cir::Return(cvalue) => self.generate_return_stmt(&cvalue)?,
+                Cir::If(cvalue, stmts_cir) => self.generate_if_stmt(cvalue, stmts_cir)?,
+                Cir::Else(stmts_cir) => self.generate_else_stmt(stmts_cir)?,
+                Cir::SubProgDef {
+                    name,
+                    return_type,
+                    stmts_cir,
+                    cparams,
+                } => {
+                    self.generate_subprogdef_stmt(
+                        name.to_string(),
+                        cparams,
+                        &return_type,
+                        stmts_cir,
+                    )?;
+                }
+            }
         }
-        CType::String => {
-            format!("\"%s\", {cvalue}")
-        }
-    };
-    writeln!(sink, "printf({write_value});")?;
-    Ok(())
+        Ok(())
+    }
+
+    pub fn generate_c_code(self: &mut Self, ir: Vec<Cir>) -> Result<String, std::fmt::Error> {
+        self.generate_prelude()?;
+        self.generate_stmts(ir)?;
+        Ok(self.sink.clone())
+    }
 }
 
-fn generate_return_stmt(sink: &mut impl Write, cvalue: &CValue) -> Result {
-    writeln!(sink, "return {cvalue};")?;
-    Ok(())
-}
-
-fn generate_subprogdef_stmt(
-    sink: &mut impl Write,
-    name: String,
-    cparams: Vec<CParam>,
-    return_type: &CType,
-    stmts: Vec<Cir>,
-) -> Result {
-    write!(sink, "{return_type} {name}")?;
-
-    write!(sink, "(")?;
-    let param_strings: Vec<String> = cparams
-        .iter()
-        .map(|param| format!("{} {}", param.param_type, param.name))
-        .collect();
-    write!(sink, "{}", param_strings.join(", "))?;
-    write!(sink, ")")?;
-
-    writeln!(sink, "{{")?;
-    generate_stmts(sink, stmts)?;
-    writeln!(sink, "}}")?;
-    Ok(())
-}
-
-// fn generate_subprogcall_stmt(sink: &mut impl Write, name: String, args: Vec<Expr>) -> Result {
+// fn generate_subprogcall_stmt(sink: &mut impl Write, name: String, args: Vec<Expr>) -> fmt::Result {
 //     let args_str = args
 //         .iter()
 //         .map(|arg| arg.to_string())
@@ -61,47 +117,9 @@ fn generate_subprogdef_stmt(
 //     Ok(())
 // }
 //
-fn generate_if_stmt(sink: &mut impl Write, expr: CValue, stmts: Vec<Cir>) -> Result {
-    writeln!(sink, "if ({expr}) {{")?;
-    generate_stmts(sink, stmts)?;
-    writeln!(sink, "}}")?;
-    Ok(())
-}
 //
-fn generate_else_stmt(sink: &mut impl Write, stmts: Vec<Cir>) -> Result {
-    writeln!(sink, "}}")?;
-    writeln!(sink, "else {{")?;
-    generate_stmts(sink, stmts)?;
-    Ok(())
-}
 //
-// fn generate_set_stmt(sink: &mut impl Write, name: String, var_type: Type, expr: Expr) -> Result {
+// fn generate_set_stmt(sink: &mut impl Write, name: String, var_type: Type, expr: Expr) -> fmt::Result {
 //     writeln!(sink, "{var_type} {name} = {expr};")?;
 //     Ok(())
 // }
-
-fn generate_stmts(sink: &mut impl Write, stmts: Vec<Cir>) -> Result {
-    for stmt in stmts {
-        match stmt {
-            Cir::Write(ctype, cvalue) => generate_write_stmt(sink, &ctype, &cvalue)?,
-            Cir::Return(cvalue) => generate_return_stmt(sink, &cvalue)?,
-            Cir::If(cvalue, stmts_cir) => generate_if_stmt(sink, cvalue, stmts_cir)?,
-            Cir::Else(stmts_cir) => generate_else_stmt(sink, stmts_cir)?,
-            Cir::SubProgDef {
-                name,
-                return_type,
-                stmts_cir,
-                cparams,
-            } => {
-                generate_subprogdef_stmt(sink, name.to_string(), cparams, &return_type, stmts_cir)?;
-            }
-        }
-    }
-    Ok(())
-}
-
-pub fn generate_c_code(sink: &mut impl Write, ir: Vec<Cir>) -> Result {
-    generate_prelude(sink)?;
-    generate_stmts(sink, ir)?;
-    Ok(())
-}
