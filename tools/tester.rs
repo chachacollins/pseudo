cb35b7d2756bc1b8a4e7d9bb0979de8ebe64e939
chachacollins <collinschacha@hotmail.com>
2025-10-09 20:56:34 +0300

    feat: added a tester to the project

--- tools/tester.rs
+++ tools/tester.rs
@@ -1,6 +1,42 @@
-use std::env;
+use std::path::Path;
+use std::process::Command;
+use std::{env, fs, io};
 
-fn main() {
+fn main() -> io::Result<()> {
     let args = env::args().collect::<Vec<String>>();
-    println!("{args:?}");
+    if args.len() < 2 {
+        eprintln!("Pass the folder to run the tests from");
+        std::process::exit(1);
+    }
+
+    let mut i = 1;
+    let dir_path = &args[1];
+    for entry in fs::read_dir(dir_path)? {
+        let entry = entry?;
+        let file_path = &entry.path();
+        let file_path = Path::new(file_path);
+        let extension = file_path.extension();
+        match extension {
+            Some(ext) => {
+                let ext = ext.to_str().unwrap();
+                if ext == "pseudo" {
+                    let file_path = file_path.to_str().unwrap();
+                    eprint!("\rRunning test: {i} file: {file_path}                           ");
+                    i += 1;
+                    let output = Command::new("cargo")
+                        .args(["pseudo", file_path])
+                        .output()
+                        .expect("Failed to run cargo pseudo command");
+                    if !output.status.success() {
+                        eprintln!("Example {} failed test because of : ", file_path);
+                        eprintln!("{}", String::from_utf8_lossy(&output.stderr));
+                        std::process::exit(1);
+                    }
+                }
+            }
+            None => {}
+        }
+    }
+    eprintln!("\r\nDone! All tests passed succesfully!");
+    Ok(())
 }
