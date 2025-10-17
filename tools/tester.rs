97bc5083afd99fd02a95e64bfc9531625fa1dd23
chachacollins <collinschacha@hotmail.com>
2025-10-17 22:53:00 +0300

    feat: made pretty printing for the tools

--- tools/tester.rs
+++ tools/tester.rs
@@ -2,6 +2,65 @@ use std::path::Path;
 use std::process::Command;
 use std::{env, fs, io};
 
+enum LogLevel {
+    Success,
+    Error,
+}
+
+fn pretty_print(msg: &str, level: LogLevel) {
+    const GREEN: &str = "\x1b[32m";
+    const RED: &str = "\x1b[31m";
+    const RESET: &str = "\x1b[0m";
+    match level {
+        LogLevel::Success => eprintln!("{GREEN}{msg}{RESET}"),
+        LogLevel::Error => eprintln!("{RED}{msg}{RESET}"),
+    }
+}
+
+fn get_output_path(input_file_path: &str) -> String {
+    let mut acc = Vec::new();
+    input_file_path.split("/").for_each(|s| acc.push(s));
+    let last = acc.pop().unwrap().split(".").next().unwrap();
+    acc.push(last);
+    return acc.join("/");
+}
+
+fn run_test(file_path: &str) {
+    eprint!("\rCompiling file {file_path}                              ");
+    let output = Command::new("cargo")
+        .args(["pseudo", file_path, "--keep"])
+        .output()
+        .expect("Failed to run cargo pseudo command");
+    if !output.status.success() {
+        pretty_print(
+            &format!("Example {} failed test because of : ", file_path),
+            LogLevel::Error,
+        );
+        pretty_print(
+            &format!("{}", String::from_utf8_lossy(&output.stderr)),
+            LogLevel::Error,
+        );
+        std::process::exit(1);
+    }
+    let executable_path = get_output_path(file_path);
+
+    eprint!("\rRunning file {executable_path}                              ");
+    let output = Command::new(executable_path)
+        .output()
+        .expect(&format!("Failed to execute {file_path} command"));
+    if !output.status.success() {
+        pretty_print(
+            &format!("Example {} failed test because of : ", file_path),
+            LogLevel::Error,
+        );
+        pretty_print(
+            &format!("{}", String::from_utf8_lossy(&output.stderr)),
+            LogLevel::Error,
+        );
+        std::process::exit(1);
+    }
+}
+
 fn main() -> io::Result<()> {
     let args = env::args().collect::<Vec<String>>();
     if args.len() < 2 {
@@ -9,6 +68,9 @@ fn main() -> io::Result<()> {
         std::process::exit(1);
     }
 
+    const HIDE_CURSOR: &str = "\x1b[?25l";
+    const UNHIDE_CURSOR: &str = "\x1b[?25h";
+    eprint!("{HIDE_CURSOR}");
     let mut i = 1;
     let dir_path = &args[1];
     for entry in fs::read_dir(dir_path)? {
@@ -21,22 +83,17 @@ fn main() -> io::Result<()> {
                 let ext = ext.to_str().unwrap();
                 if ext == "pseudo" {
                     let file_path = file_path.to_str().unwrap();
-                    eprint!("\rRunning test: {i} file: {file_path}                           ");
+                    eprint!(
+                        "\rRunning test: {i} on file: {file_path}                           \n"
+                    );
                     i += 1;
-                    let output = Command::new("cargo")
-                        .args(["pseudo", file_path, "--keep"])
-                        .output()
-                        .expect("Failed to run cargo pseudo command");
-                    if !output.status.success() {
-                        eprintln!("Example {} failed test because of : ", file_path);
-                        eprintln!("{}", String::from_utf8_lossy(&output.stderr));
-                        std::process::exit(1);
-                    }
+                    run_test(file_path);
                 }
             }
             None => {}
         }
     }
-    eprintln!("\r\nDone! All tests passed succesfully!");
+    pretty_print("\rDone! All tests passed succesfully!", LogLevel::Success);
+    eprint!("{UNHIDE_CURSOR}");
     Ok(())
 }
